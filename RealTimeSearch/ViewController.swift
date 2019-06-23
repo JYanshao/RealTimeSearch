764ed71844b1c2d320b763550767f6d171cd1749
suguangyao <654565181@qq.com>
2019-06-23 19:18:38 +0800

    first

---
+++ RealTimeSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealTimeSearch
+//
+//  Created by artron on 2019/6/22.
+//  Copyright © 2019 artron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
